5d8022da77bd3b57b1dd6f97ca2d023fcbf23887
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-25 21:46:42 +0600

    project created with required dependencies

---
+++ src/main/java/io/murad/MichelleFajardo_301097601_MTTest/MichelleFajardo301097601MtTestApplication.java
@@ -0,0 +1,13 @@
+package io.murad.MichelleFajardo_301097601_MTTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichelleFajardo301097601MtTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichelleFajardo301097601MtTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/murad/MichelleFajardo_301097601_MTTest/MichelleFajardo301097601MtTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.murad.MichelleFajardo_301097601_MTTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MichelleFajardo301097601MtTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
